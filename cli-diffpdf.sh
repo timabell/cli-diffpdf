a0b1fad11e5ea0b2cc975bf4e9346d02fc8ddbca
tpltnt <tpltnt.github@dropcut.net>
2013-04-25 21:25:01 +0200

    wdiff test added, redirection removed

--- cli-diffpdf.sh
+++ cli-diffpdf.sh
@@ -3,13 +3,19 @@
 # check for two arguments being given
 if [ $# -ne 2 ]
 then
-    echo "usage: cli-diffpdf file1.pdf file2.pdf" &>2
+    echo "usage: cli-diffpdf file1.pdf file2.pdf"
     exit 1
 fi
 
 # check for tools needed
 if [ -z `which pdftotext` ]
 then
-    echo "please install pdftotext/xpdf" &>2
+    echo "please install pdftotext/xpdf"
+    exit 1
+fi
+
+if [ -z `which wdiff` ]
+then
+    echo "please install wdiff!"
     exit 1
 fi
