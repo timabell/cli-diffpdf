a1af90a09abc42667be29f1d31c14dded78ee6d5
tpltnt <tpltnt.github@dropcut.net>
2013-04-25 21:11:36 +0200

    test for right number of arguments added

--- cli-diffpdf.sh
+++ cli-diffpdf.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# check for two arguments being given
+if [ $# -ne 2 ]
+then
+    echo "usage: cli-diffpdf file1.pdf file2.pdf"
+    exit 1
+fi
+
 # check for tools needed
 if [ -z `which pdftotext` ]
 then
