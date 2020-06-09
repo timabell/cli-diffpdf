bb0097d56a5adf82fb19ae170b3d54ecfba4ba66
Tim Abell <tim@timwise.co.uk>
2020-06-09 21:29:34 +0100

    use kdiff3 instead

--- cli-diffpdf.sh
+++ cli-diffpdf.sh
@@ -14,22 +14,12 @@ then
     exit 1
 fi
 
-if [ -z `which wdiff` ]
-then
-    echo "please install wdiff!"
-    exit 1
-fi
-
-
 # dump pdfs into tests
 pdftotext -layout "$1" file1.tmp
 pdftotext -layout "$2" file2.tmp
 
 # compare the two textdumps
-RED=$'\e'"[1;31m"
-GREEN=$'\e'"[1;32m"
-RESET=$'\e'"[0m"
-wdiff -w$RED -x$RESET -y$GREEN -z$RESET -n file1.tmp file2.tmp
+kdiff3 file1.tmp file2.tmp
 
 # clean up temporary files
 rm file1.tmp
