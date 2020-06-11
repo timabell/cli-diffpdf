30dbff64967e8dfb77df0469266f114f91044c7a
Tim Abell <tim@timwise.co.uk>
2020-06-11 11:13:11 +0100

    strip out docusign watermarks

--- cli-diffpdf.sh
+++ cli-diffpdf.sh
@@ -21,8 +21,8 @@ cd tmp
 pdftotext -layout "$1" file1.tmp
 pdftotext -layout "$2" file2.tmp
 
-egrep -v 'Page.*of|DocuSign Envelope|^$' file1.tmp > file1-filtered.tmp
-egrep -v 'Page.*of|DocuSign Envelope|^$' file2.tmp > file2-filtered.tmp
+egrep -v 'Page.*of|DocuSign Envelope|^                                    In Process$|^$' file1.tmp | sed 's/In Process/          /g' > file1-filtered.tmp
+egrep -v 'Page.*of|DocuSign Envelope|^                                    In Process$|^$' file2.tmp | sed 's/In Process/          /g' > file2-filtered.tmp
 
 # compare the two textdumps
 kdiff3 file1-filtered.tmp file2-filtered.tmp &
